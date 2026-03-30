d5a11432bf749c4e64401279922da6f84e055c3b
evellyn severo <evellyn.severo@zup.com.br>
2026-03-30 15:04:08 -0300

    feat: ajustes finais na classe ApiResponse

--- src/main/java/com/zup/openAPI/models/ApiResponse.java
+++ src/main/java/com/zup/openAPI/models/ApiResponse.java
@@ -1,4 +1,49 @@
 package com.zup.openAPI.models;
 
-public class ApiResponse {
+public class ApiResponse <T> {
+    private int status;
+    private String message;
+    private T data;
+    private String error;
+
+    public ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.error = null;
+    }
+    public ApiResponse(int status, String message, String error) {
+        this.status = status;
+        this.message = message;
+        this.data = null;
+        this.error = error;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+    public String getError() { return error; }
+    public void setError(String error) { this.error = error; }
 }
+
+
