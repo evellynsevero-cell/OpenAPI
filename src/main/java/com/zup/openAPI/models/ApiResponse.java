f3819d53498fae37d35f379a3d5dea465747b9e4
evellyn severo <evellyn.severo@zup.com.br>
2026-03-30 13:14:30 -0300

    feat: cria classe ApiResponse com status, message e data

--- src/main/java/com/zup/openAPI/models/ApiResponse.java
+++ src/main/java/com/zup/openAPI/models/ApiResponse.java
@@ -1,4 +1,17 @@
 package com.zup.openAPI.models;
 
-public class ApiResponse {
+
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+    private String error;
+
+    public ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+
+    }
+
 }
