440a3369fbe026d754600a926c899c3681b94676
evellyn severo <evellyn.severo@zup.com.br>
2026-03-30 15:05:30 -0300

    feat: adicionado, exception, handler

---
+++ src/main/java/com/zup/openAPI/exceptions/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package com.zup.openAPI.exceptions;
+
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(Long id) {
+        super("Tarefa com id=" + id + " não encontrada");
+    }
+}
+
