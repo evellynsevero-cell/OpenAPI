b69214d5f98937086e03afd756c7125ab9d9a346
evellyn severo <evellyn.severo@zup.com.br>
2026-03-30 13:16:26 -0300

    feat: implementa TaskController com endpoints CRUD, filtros e paginação

--- src/main/java/com/zup/openAPI/controllers/TaskController.java
+++ src/main/java/com/zup/openAPI/controllers/TaskController.java
@@ -15,21 +15,6 @@ import java.util.stream.Collectors;
 @RequestMapping("/v1/tasks")
 @Tag(name = "Tasks", description = "Gerenciamento de tarefas")
 
-import com.zup.openAPI.exceptions.TaskNotFoundException;
-import com.zup.openAPI.models.Task;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/v1/tasks")
-@Tag(name = "Tasks", description = "Gerenciamento de tarefas")
-
 public class TaskController {
     private final List<Task> tasks = new ArrayList<>();
     private long nextId = 1;
