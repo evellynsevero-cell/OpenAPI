93f3b959f3ca0fe066d90a2679e40643c4d6f8ee
evellyn severo <evellyn.severo@zup.com.br>
2026-03-30 13:05:41 -0300

    feat: adiciona campo description à classe Task

--- src/main/java/com/zup/openAPI/models/Task.java
+++ src/main/java/com/zup/openAPI/models/Task.java
@@ -1,4 +1,48 @@
 package com.zup.openAPI.models;
 
 public class Task {
-}
+        private Long id;
+        private String title;
+        private boolean done;
+        private String description;
+
+        public Task() {}
+
+        public Task(Long id, String title, boolean done, String description) {
+            this.id = id;
+            this.title = title;
+            this.done = done;
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public boolean isDone() {
+            return done;
+        }
+
+        public void setDone(boolean done) {
+            this.done = done;
+        }
+    }
